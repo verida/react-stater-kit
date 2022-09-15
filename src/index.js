0b050fc16a6f16b1fb24ba5d112e35a19a4e40d8
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2022-09-15 06:21:58 +0100

    feat: added config to support nodejs modules that is not supported in webpack 5

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./assets/css/index.css"
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import "./assets/fonts/Sora-Regular.ttf"
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
